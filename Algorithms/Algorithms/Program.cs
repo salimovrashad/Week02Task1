de499834db4c87e480a28f3ca475760a1e348ba7
salimovrashad <selimovresad33@gmail.com>
2023-10-12 21:59:07 +0400

    add task file

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //Console.WriteLine("Input the number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int[] arr = new int[9999999];
+            //arr[0] = 0;arr[1] = 1;arr[2] = 1;
+
+            //for (int i = 2; i < arr.Length; i++)
+            //{
+            //    arr[i] = arr[i - 1] + arr[i - 2];
+                
+            //    Console.WriteLine(arr[i]);
+            //    if (number <= arr[i])
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //Task 2
+            //Console.WriteLine("Input the first number: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input the second number: ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //b -= a;
+            //a += b;
+            //b = a - b;
+            //Console.WriteLine("First Number: " + a);
+            //Console.WriteLine("Second Number: " + b);
+        }
+    }
+}
